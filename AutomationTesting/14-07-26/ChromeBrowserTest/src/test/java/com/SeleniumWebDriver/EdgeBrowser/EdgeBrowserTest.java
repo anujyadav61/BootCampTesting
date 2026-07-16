8f8bbbfd786cbbfc9a7d7d1f7c2780a4f99d0f4d
anujyadav61 <yadavanuj151005@gmail.com>
2026-07-16 23:44:55 +0530

    Add files via upload

---
+++ AutomationTesting/14-07-26/ChromeBrowserTest/src/test/java/com/SeleniumWebDriver/EdgeBrowser/EdgeBrowserTest.java
@@ -0,0 +1,14 @@
+package com.SeleniumWebDriver.EdgeBrowser;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowserTest {
+
+	public static void main(String[] args) {
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("https://www.selenium.dev/");
+		driver.manage().window().maximize();
+
+	}
+
+}
---
+++ AutomationTesting/14-07-26/Selenium_WebDriver_Launch_Chrome_Browser/src/com/Seleniuim_Webdriver_Chrome_Browser/ChromeBrowserTest.java
@@ -0,0 +1,13 @@
+package com.Seleniuim_Webdriver_Chrome_Browser;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserTest {
+
+	public static void main(String[] args) {
+		ChromeDriver Driver = new ChromeDriver();
+		
+
+	}
+
+}
